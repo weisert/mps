d2cd8fecd5162f09858b2423d5ef97dd51eef97e
Igor Vayzert <igor.weisert@gmail.com>
2016-04-27 22:34:37 +0600

    Video with thumbnails draft.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,11 +7,5 @@ angular.module('mpsApp')
                 videoList.sort(function(a, b) { return a.name < b.name; });
                 $scope.videoList = videoList;
             });
-
-            $scope.showFiles = function($event, item) {
-                var body = $($event.currentTarget).closest('.panel').find('.panel-body');
-                body.fadeToggle();
-                body.removeClass('initiallyHidden');
-            };
         }
     ]);
